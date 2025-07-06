cfaa30f271fb517cbe4fbcff3b0c9144c0ef3ead
TheRodrig0 <rodrigogeronimo11@gmail.com>
2025-07-06 13:07:10 -0300

    Refactor AppInterface to simplify method signatures and remove HttpRequestType type definition; delete unused http-request-type.ts file

--- backend/src/types/app-interface.ts
+++ backend/src/types/app-interface.ts
@@ -1,21 +1,16 @@
-import type { HttpRequestType } from "./common/http-request-type"
 import type { ReplyInterface } from "./common/reply-interface"
 import type { RequestInterface } from "./common/request-interface"
 
+type HttpRequestType = <Body, Param>(
+    path: string,
+    handler: (request: RequestInterface<Body, Param>, reply: ReplyInterface) => void
+) => void
+
 export interface AppInterface {
-    listen(
-        opts: { port: number },
-        callback?: (error: Error | null, address?: string) => void
-    ): void
-    register(
-        plugin: Function,
-        opts?: Record<string, string | number | boolean | string[]>
-    ): Promise<void>
-    ready(): Promise<void>
+    listen(opts: { port: number }): Promise<string>
+    register(plugin: unknown, opts?: Record<string, unknown>): Promise<void>
     printRoutes(): string
-    setErrorHandler(
-        handler: (error: Error, request: RequestInterface, reply: ReplyInterface) => void
-    ): void
+    setErrorHandler(handler: (error: Error & { statusCode: number }, request: RequestInterface, reply: ReplyInterface) => void): void
     get: HttpRequestType
     post: HttpRequestType
     patch: HttpRequestType
--- backend/src/types/common/http-request-type.ts
+++
@@ -1,7 +0,0 @@
-import { RequestInterface } from "./request-interface"
-import { ReplyInterface } from "./reply-interface"
-
-export type HttpRequestType = <Body = unknown>(
-    path: string,
-    handler: (request: RequestInterface<Body>, reply: ReplyInterface) => void
-) => void
