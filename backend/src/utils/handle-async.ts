d42c69d28b14bf9c8f3a105af3273a8d19d07197
TheRodrig0 <rodrigogeronimo11@gmail.com>
2025-07-06 13:07:38 -0300

    Remove handleAsync utility function as it is no longer needed

--- backend/src/utils/handle-async.ts
+++
@@ -1,8 +0,0 @@
-export async function handleAsync<T>(promise: Promise<T>): Promise<[Error | null, T | null]> {
-    try {
-        const data = await promise
-        return [null, data]
-    } catch (error) {
-        return [error as Error, null]
-    }
-}
