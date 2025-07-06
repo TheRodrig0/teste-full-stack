6c276d6d083ca34a14129bd90d9786f0f61d0f22
TheRodrig0 <rodrigogeronimo11@gmail.com>
2025-07-06 13:05:58 -0300

    Refactor TaskController to simplify response handling and update DTO types for task operations

--- backend/src/controllers/task-controller.ts
+++ backend/src/controllers/task-controller.ts
@@ -1,91 +1,47 @@
-import type { ReplyInterface } from "../types/common/reply-interface"
-import type { RequestInterface } from "../types/common/request-interface"
 import type { CrudControllerInterface } from "../types/crud-controller-interface"
 import type { CrudServiceInterface } from "../types/crud-service-interface"
-import type {
-    CreateTaskDTO,
-    UpdateTaskDTO,
-    ReplyTaskDTO
-} from "../types/task-dtos"
-import { handleAsync } from "../utils/handle-async"
+import type { ReplyInterface } from "../types/common/reply-interface"
+import type { RequestInterface } from "../types/common/request-interface"
+import type { Task, CreateTask, UpdateTask } from "../types/task-dtos"
 
-export class TaskController implements CrudControllerInterface<CreateTaskDTO> {
-    constructor(private readonly service: CrudServiceInterface<CreateTaskDTO, UpdateTaskDTO, ReplyTaskDTO>) { }
+export class TaskController implements CrudControllerInterface<CreateTask, UpdateTask> {
+    constructor(private readonly service: CrudServiceInterface<CreateTask, UpdateTask, Task>) { }
 
-    async findAll(_request: RequestInterface, reply: ReplyInterface): Promise<ReplyInterface> {
-        const [error, data] = await handleAsync(this.service.findAll())
+    async findAll(request: RequestInterface, reply: ReplyInterface): Promise<void> {
+        const tasks = await this.service.findAll()
 
-        if (error) {
-            return reply.code(400)
-                .send({
-                    message: (error as Error).message
-                })
-        }
-
-        return reply.code(200)
-            .send({
-                data
-            })
+        reply.status(200)
+            .send({ data: tasks })
     }
 
-    async findById(request: RequestInterface, reply: ReplyInterface): Promise<ReplyInterface> {
+    async findById(request: RequestInterface<unknown, { id: string }>, reply: ReplyInterface): Promise<void> {
         const { id } = request.params
-        const [error, data] = await handleAsync(this.service.findById(id))
+        const task = await this.service.findById(id)
 
-        if (error) {
-            return reply.code(400)
-                .send({
-                    message: (error as Error).message
-                })
-        }
-
-        return reply.code(200)
-            .send({
-                data
-            })
+        reply.status(200)
+            .send({ data: task })
     }
 
-    async create(request: RequestInterface<CreateTaskDTO>, reply: ReplyInterface): Promise<ReplyInterface> {
-        const [error, data] = await handleAsync(this.service.create(request.body))
+    async create(request: RequestInterface<CreateTask>, reply: ReplyInterface): Promise<void> {
+        const newTask = await this.service.create(request.body)
 
-        if (error) {
-            return reply.code(400)
-                .send({
-                    message: (error as Error).message
-                })
-        }
-
-        return reply.code(201)
-            .send({
-                data
-            })
+        reply.status(201)
+            .send({ data: newTask })
     }
 
-    async update(request: RequestInterface<UpdateTaskDTO>, reply: ReplyInterface): Promise<ReplyInterface> {
+    async update(request: RequestInterface<UpdateTask, { id: string }>, reply: ReplyInterface): Promise<void> {
         const { id } = request.params
-        const [error, data] = await handleAsync(this.service.update(id, request.body))
+        const updatedTask = await this.service.update(id, request.body)
 
-        if (error) {
-            return reply.code(400)
-                .send({ message: (error as Error).message })
-        }
-
-        return reply.code(200)
-            .send({
-                data
-            })
+        reply.status(200)
+            .send({ data: updatedTask })
     }
 
-    async delete(request: RequestInterface, reply: ReplyInterface): Promise<ReplyInterface> {
+    async delete(request: RequestInterface<unknown, { id: string }>, reply: ReplyInterface): Promise<void> {
         const { id } = request.params
-        const [error] = await handleAsync(this.service.delete(id))
+        await this.service.delete(id)
 
-        if (error) {
-            return reply.code(400)
-                .send({ message: (error as Error).message })
-        }
-
-        return reply.code(204)
+        reply.status(204)
             .send()
     }
 }
